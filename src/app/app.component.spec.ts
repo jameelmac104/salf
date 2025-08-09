a8439d9b2a7a89eb37102a95ce320c790aa075f4
mohamed-jameel <jameel@ideassion.com>
2025-08-09 23:13:55 +0530

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sal' title`, () => {
+  it(`should have the 'salf' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sal');
+    expect(app.title).toEqual('salf');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, salf');
   });
 });
