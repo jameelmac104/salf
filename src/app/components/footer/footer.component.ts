28c8799018386226e4c70a1074c59566fc134ff7
mohamed-jameel <jameel@ideassion.com>
2025-09-09 00:40:17 +0530

    contact page and style changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink} from "@angular/router";
 
 
@@ -10,5 +10,6 @@ import { RouterLink} from "@angular/router";
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  @Input() contactPage: boolean = false;
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [CommonModule, FooterComponent, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+
+  contact = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  private readonly TO_EMAIL = 'johnpaul@southasialawfirm.com';
+  sendMail() {
+    const { name, email, subject, message } = this.contact;
+
+    const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0A%0D%0AMessage:%0D%0A${message}`;
+    const mailtoHref = `mailto:${this.TO_EMAIL}?subject=${encodeURIComponent(subject)}&body=${body}`;
+
+    window.location.href = mailtoHref;
+  }
 
 }
